6f2accf7bd45ba81f03d5ddfd75e36533c419090
Jester <jester@Jesters-MacBook.local>
2019-03-05 08:59:12 +0100

    added scss boilerplate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import './scss/main.scss';
-import './sass/main.sass';
-
-console.log('this is a fucking boilerplate');
 
 const arr = [1, 2, 3];
 const iAmJavascriptES6 = () => console.log(...arr);
